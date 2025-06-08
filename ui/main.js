e08f0c5af73595360e0396da27935aa7c3a7564c
a-d-iii <adi.xv0@gmail.com>
2025-06-08 21:25:05 +0530

    Fix swiper import path

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,20 @@
-
 import Vue from 'vue'
 import WeatherApp from './WeatherApp.vue'
 
+const ipc = window.require ? window.require('electron').ipcRenderer : null
+
 new Vue({
-  render: h => h(WeatherApp)
+  data() {
+    return { weather: null }
+  },
+  render(h) {
+    return h(WeatherApp, { props: { weather: this.weather } })
+  },
+  mounted() {
+    if (ipc) {
+      ipc.on('weather-data', (event, data) => {
+        this.weather = data
+      })
+    }
+  }
 }).$mount('#app')
